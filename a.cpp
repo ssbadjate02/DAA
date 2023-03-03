9f9ba5c5c92f8a9085e8abaa5cf44e5d673e0d13
AnkitSama <88831151+AnkitSama@users.noreply.github.com>
2023-03-04 00:40:11 +0530

    DCEL implemented for input polygon

--- a.cpp
+++ a.cpp
@@ -30,8 +30,7 @@ public:
     Vertex *start;
     Vertex *end;
     Edge *twin;
-    Face *left;
-    Face *right;
+    Face *face;
     Edge *prev;
     Edge *next;
     Edge(Vertex *start, Vertex *end)
@@ -41,8 +40,7 @@ public:
         twin = NULL;
         prev = NULL;
         next = NULL;
-        left = NULL;
-        right = NULL;
+        face = NULL;
     }
 };
 
@@ -50,6 +48,7 @@ class Face
 {
 public:
     Edge *inc_edge;
+
 };
 
 class DCEL
@@ -66,10 +65,11 @@ public:
             vertices.push_back(curr);
         }
 
-        for (int i = 0; i < points.size(); i++)
+        int n = points.size();
+        for (int i = 0; i < n; i++)
         {
             Vertex *v1 = find_vertex(points[i].first, points[i].second);
-            Vertex *v2 = find_vertex(points[(i + 1) % points.size()].first, points[(i + 1) % points.size()].second);
+            Vertex *v2 = find_vertex(points[(i + 1) % n].first, points[(i + 1) % n].second);
 
             Edge *e1 = new Edge(v1, v2);
             Edge *e2 = new Edge(v2, v1);
@@ -78,10 +78,27 @@ public:
             e2->twin = e1;
 
             v1->inc_edge = e1;
-            v2->inc_edge = e2;
 
             edges.push_back(e1);
             edges.push_back(e2);
+        }
+
+        n = edges.size();
+        faces.push_back(new Face());
+        faces.push_back(new Face());
+
+        faces[0]->inc_edge = edges[0];
+        faces[1]->inc_edge = edges[1];
+        for(int i = 0; i<n; i += 2)
+        {
+            edges[i]->next = edges[(i+2)%n];
+            edges[(i+2)%n]->prev = edges[i];
+
+            edges[(i+1)%n]->prev = edges[(i+3)%n];
+            edges[(i+3)%n]->next = edges[(i+1)%n];
+
+            edges[i]->face = faces[0];
+            edges[(i+1)%n]->face = faces[1];
         }
     }
 
@@ -94,16 +111,56 @@ public:
         }
         return NULL;
     }
+    /*
+    HELPER FUNCTIONS
+    */
 
-    Edge *find_edge(Vertex *v1, Vertex *v2)
+    void print_vertex(Vertex* v)
     {
-        for (Edge *edge : edges)
-        {
-            if ((v1->x == edge->start->x && v1->y == edge->start->y) && (v2->x == edge->end->x && v2->y == edge->end->y))
-                return edge;
-        }
-        return NULL;
+        cout<<v->x<<" "<<v->y<<" "<<find_edge_index(v->inc_edge)<<"\n";
     }
+
+    int find_edge_index(Edge* e1)
+    {
+        for(int i = 0; i<edges.size(); i++)
+        {
+            if(e1 == edges[i])
+                return i;
+        }
+        return -1;
+    }
+
+    int find_face_index(Face* f)
+    {
+        for(int i = 0;i<faces.size();i++)
+        {
+            if(f == faces[i])
+                return i;
+        }
+        return -1;
+    }
+    void print()
+    {
+        cout<<"Vertex : \n";
+        for(Vertex* v : vertices)
+            print_vertex(v);
+        cout<<"\n";
+        cout<<"Edge : \n";
+        for(Edge* e : edges)
+        {
+            cout<<"Start : ";
+            print_vertex(e->start);
+            cout<<"End : ";
+            print_vertex(e->end);
+            cout<<"Twin : "<<find_edge_index(e->twin)<<"\n";
+            cout<<"Prev : "<<find_edge_index(e->prev)<<"\n";
+            cout<<"Next : "<<find_edge_index(e->next)<<"\n";
+            cout<<"Face : "<<find_face_index(e->face)<<"\n";
+            cout<<"\n";
+        }
+    }
+    /*
+    */
 };
 
 int main()
@@ -111,5 +168,6 @@ int main()
     FASTIO;
     vector<pair<double, double>> points = {{0, 0}, {0, 1}, {1, 1}, {1, 0}};
     DCEL *polygon = new DCEL(points);
+    polygon->print();
     return 0;
 }
