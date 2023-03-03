43c673a02d289bbf59cf502e45670f194d71f3fb
“ssbadjate02” <ssbadjate02@gmail.com>
2023-03-03 22:21:25 +0530

    DAA

---
+++ a.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define all(x) (x).begin(),(x).end()
+#define ss second
+#define ff first
+
+class Vertex{
+    int x,y;
+    Edge *inc_edge;
+};
+
+class Edge{
+    Vertex *start;
+    Vertex *end;
+    Edge *twin;
+    Face *left;
+    Face *right;
+    Edge *prev;
+    Edge *next;
+};
+
+class Face{
+    Edge *inc_edge;
+};
+
+
+int main()
+{
+    FASTIO;
+     
+}
